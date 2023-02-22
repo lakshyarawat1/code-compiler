4da24d14e2d17c1d6a8d32eb0414685dffad56b2
lakshyarawat1 <lakshya.paramount@gmail.com>
2023-02-22 17:13:10 +0530

    added monaco compiler

--- client/compiler/src/App.jsx
+++ client/compiler/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import MonacoEditor from 'react-monaco-editor'
 
 function App() {
 
   return (
     <div className="App">
-
+      <MonacoEditor
+        width='800'
+        height='600'
+        language='javascript'
+        theme='vs-dark'
+      />
     </div>
   )
 }
